e32759c1d964db48a219825949c3d4da1ee3c842
Robin <vougishtiger187@gmail.com>
2024-06-28 12:12:49 -0700

    main

---
+++ index.js
@@ -0,0 +1,14 @@
+// Create a string saying "You have received this message because you have been chosen to open an important vault. Here is the secret combination."
+
+const message=`You have received this message because you have been chosen to open an important vault. Here is the secret combination`;
+
+// assign 3 variables using a unique arithmetic operator(+-*/). Each must equal one of three codes in the combination. 
+const firstNumber=8+2
+const secondNumber=4*10
+const thirdNumber=35+4
+
+
+
+// Create a popup dialog box displaying the vault codes and the text.
+
+alert(`${message} ${firstNumber} ${secondNumber} ${thirdNumber}`);
